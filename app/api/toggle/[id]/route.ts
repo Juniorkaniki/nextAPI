f48de7ddbea8150caef34455d06a86c6820fb065
Juniorkaniki <juniorkaniki00@gmail.com>
2026-02-12 22:47:07 +0100

    modification

--- app/api/toggle/[id]/route.ts
+++ app/api/toggle/[id]/route.ts
@@ -2,3 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/db/drizzle";
 import { todos } from "@/db/schema";
 import { eq } from "drizzle-orm";
+
+
+export async function PATCH(req:NextRequest,
+    {params} : {params: { id :string}}
+)  {
+    const id =Number (params.id);
+    if (isNaN(id)){
+        return NextResponse.json({error : "Unvalid ID"},{status :400});
+    }
+}
